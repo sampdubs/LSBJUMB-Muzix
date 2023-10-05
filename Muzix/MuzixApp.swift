d7d2e2e607bf1070beb218848168a8a5f72f3e9a
sampdubs <sammypw1@gmail.com>
2023-10-05 13:19:20 -0700

    Initial Commit

---
+++ Muzix/MuzixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuzixApp.swift
+//  Muzix
+//
+//  Created by Sam Prausnitz-Weinbaum on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MuzixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
