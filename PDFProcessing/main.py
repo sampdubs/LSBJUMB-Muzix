c01e94413ee55a48203132a96098db5efc69dcb8
sampdubs <sammypw1@gmail.com>
2023-10-05 15:28:20 -0700

    Python to extract individual songs from pdf

--- PDFProcessing/main.py
+++ PDFProcessing/main.py
@@ -1,7 +1,9 @@
 # importing required modules
 import PyPDF2
 import re
- 
+from tqdm import tqdm
+
+
 # creating a pdf file object
 pdfFileObj = open('TUBA muzix.pdf', 'rb')
  
@@ -11,13 +13,36 @@ pdfReader = PyPDF2.PdfReader(pdfFileObj)
 # get text from page 1
 tableOfContents = pdfReader.pages[0].extract_text()
 
-# print(tableOfContents)
+# extract song names and page numbers from table of contents
+pattern = re.compile(r"(\d+):\s+(.+?)\s*(?=\d+:|\n|\Z)")
 
-pattern = re.compile(r"\d+:\s+(.+?)(?=\s+\d+:|\n)")
+matches = re.findall(pattern, tableOfContents)
+matches = [(int(page), title) for page, title in matches]
+matches.sort()
 
-match = re.findall(pattern, tableOfContents)
-print(match)
-print(len(match))
+for page, title in tqdm(matches[:-1]):
+    # Extract appropriate page and make a new pdf with just that song
+    pdfWriter = PyPDF2.PdfWriter()
+    pageObj = pdfReader.pages[page + 1]
+
+    pdfWriter.add_page(pageObj)
+    newFile = open(f"songz/{title}.pdf", 'wb')
+
+    pdfWriter.write(newFile)
+    newFile.close()
+
+# Handle special case of teazers
+startPage, title = matches[-1]
+pdfWriter = PyPDF2.PdfWriter()
+
+for page in range(startPage + 1, startPage + 6):
+    pageObj = pdfReader.pages[page]
+    pdfWriter.add_page(pageObj)
+
+newFile = open(f"songz/{title}.pdf", 'wb')
+
+pdfWriter.write(newFile)
+newFile.close()
 
 # closing the pdf file object
 pdfFileObj.close()
