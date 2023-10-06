5d247f4684e0db2d6dc8d12bed93dceff472b0b1
sampdubs <sammypw1@gmail.com>
2023-10-05 20:38:55 -0700

    Add UI for music

--- Muzix/ContentView.swift
+++ Muzix/ContentView.swift
@@ -6,19 +6,87 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var currentSong: String?
+    private let path = Bundle.main.resourcePath! + "/Songz/"
+    
+    func getFiles() -> [String] {
+        let fm = FileManager.default
+        
+        do {
+            var items = try fm.contentsOfDirectory(atPath: path)
+            for i in 0..<items.count {
+                let index = items[i].index(items[i].endIndex, offsetBy: -5)
+                items[i] = String(items[i][...index])
+            }
+            return items
+        } catch {
+            return ["Error"]
         }
-        .padding()
+        
+    }
+    
+    var body: some View {
+        if (currentSong != nil){
+            ZStack {
+                PDFKitView(url: URL(fileURLWithPath: path + currentSong! + ".pdf"))
+                closeButton
+            }
+    
+        } else {
+            ScrollView {
+                VStack {
+                    ForEach(self.getFiles(), id: \.self) { file in
+                        Button(action: {
+                            currentSong = file
+                        }, label: {
+                            Text(file)
+                                .bold()
+                        })
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+    
+    var closeButton: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    currentSong = nil
+                }) {
+                    Image(systemName: "xmark.circle")
+                        .padding(1)
+                }
+                Spacer()
+            }
+            .padding(.top, 5)
+            Spacer()
+        }
     }
 }
+
+struct PDFKitView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: UIViewRepresentableContext<PDFKitView>) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        pdfView.autoScales = true
+        pdfView.scaleFactor = 1.6
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: PDFView, context: UIViewRepresentableContext<PDFKitView>) {
+        // TODO
+    }
+}
+
 
 #Preview {
     ContentView()
 }
+
