d5573e84d359baf07a56b5ffd2a873c0ae5bec79
sampdubs <sammypw1@gmail.com>
2024-02-09 17:34:32 -0800

    Better microphone button, add haptics

--- Muzix/Views/DictationView.swift
+++ Muzix/Views/DictationView.swift
@@ -9,41 +9,55 @@ import SwiftUI
 
 struct DictationView: View {
     @State var micPressed = false
-    //    let speechRecognizer: SpeechRecognizer
     @StateObject var speechRecognizer: SpeechRecognizer
     let recieveText: (String) -> Void
+    @State var startRecording = DispatchTime.now()
     
     var body: some View {
         VStack {
             Spacer()
             HStack {
-                Image(systemName: micPressed ? "mic.fill" : "mic")
-                    .font(.title)
-                    .padding()
-                    .foregroundStyle(.accent)
                 Spacer()
+                Group {
+                    Image(systemName: micPressed ? "mic.fill" : "mic")
+                        .font(.title)
+                        .padding()
+                        .foregroundStyle(.accent)
+                }
+                .contentShape(Rectangle())
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.accent, lineWidth: 4)
+                )
+                .padding()
+                .simultaneousGesture(
+                    DragGesture(minimumDistance: 0)
+                        .onChanged({ _ in
+                            beginRecording()
+                        })
+                        .onEnded({ _ in
+                            endRecording()
+                        })
+                )
+                .sensoryFeedback(trigger: micPressed) { old, new in
+                    return (old ? .success : .impact(flexibility: .solid, intensity: 1))
+                }
             }
-            .simultaneousGesture(
-                DragGesture(minimumDistance: 0)
-                    .onChanged({ _ in
-                        beginRecording()
-                    })
-                    .onEnded({ _ in
-                        endRecording()
-                    })
-            )
         }
     }
     
     @MainActor func beginRecording() {
         if (!micPressed) {
-            speechRecognizer.start()
             micPressed = true
+            startRecording = .now()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                speechRecognizer.start()
+            }
         }
     }
     
     @MainActor func endRecording() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        DispatchQueue.main.asyncAfter(deadline: max(startRecording.advanced(by: .seconds(2)), .now().advanced(by: .milliseconds(500)))) {
             micPressed = false
             speechRecognizer.stop()
             print("Transcript: " + speechRecognizer.text)
